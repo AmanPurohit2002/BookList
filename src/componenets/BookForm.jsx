2491d68f0a90d1f668b92e4bee0a8f30bee43d21
Aman_Purohit <amanpurohit785@gmail.com>
2022-12-19 23:56:37 +0530

    removed the bug of entering space from beginning

--- src/componenets/BookForm.jsx
+++ src/componenets/BookForm.jsx
@@ -17,13 +17,19 @@ export const BookForm=()=>{
 
     const disable=title.length ===0 && author.length===0;
 
+    // const regex=/^(?!\s).+(?<!\s)$/gm;
+
+    const handleTitle=(e)=>(setTitle(e.target.value.replace(/^\s/,"")));
+
+    const handleAuthor=(e)=>(setAuthor(e.target.value.replace(/^\s/,"")));
+
     return (
         <form onSubmit={handleSubmit}>
             <input type='text' value={title} placeholder='book title'  
-                onChange={(e)=>(setTitle(e.target.value))}
+                onChange={handleTitle}
             />
             <input type='text' value={author} placeholder='book author'  
-                onChange={(e)=>(setAuthor(e.target.value))}
+                onChange={handleAuthor}
             />
             <button disabled={disable}>Add book</button>
         </form>
